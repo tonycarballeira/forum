e4f8a1a939970e133d3c86118698442594b80bf6
tonycarballeira <tonycarballeira@gmail.com>
2015-01-20 18:41:43 -0500

    Modified ForumPost and ForumThread Models

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -1,4 +1,10 @@
 class ForumPost < ActiveRecord::Base
 	belongs_to :forum_thread
 	belongs_to :user
+
+	validates :body, presence: true
+
+	def send_notifications!
+		forum_thread.
+	end
 end
--- app/models/forum_thread.rb
+++ app/models/forum_thread.rb
@@ -1,6 +1,7 @@
 class ForumThread < ActiveRecord::Base
 	belongs_to :user
 	has_many :forum_posts
+	has_many :users, through: :forum_posts
 
 	accepts_nested_attributes_for :forum_posts
 
