9e0f9363c24f395bcc0ce1e1900bff97f7d627d2
tonycarballeira <tonycarballeira@gmail.com>
2015-01-20 18:52:17 -0500

    Added Recipient and Sender To Mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,5 +4,10 @@ class NotificationMailer < ApplicationMailer
 	def forum_post_notification(user, forum_post)
 		@user = user
 		@forum_post = forum_post
+
+		mail(
+			to: "#{user.email}"
+			subject: "[forum] New Post in #{forum_post.forum_thread.title}"
+			)
 	end
 end
