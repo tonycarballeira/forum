f0de1f377fead9555cb3e77bc76430361337ee74
tonycarballeira <tonycarballeira@gmail.com>
2015-01-20 18:49:57 -0500

    Modified Notification Mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,8 @@
 class NotificationMailer < ApplicationMailer
+	default from: "notifications@tonyforum.com"
+
+	def forum_post_notification(user, forum_post)
+		@user = user
+		@forum_post = forum_post
+	end
 end
--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -6,5 +6,9 @@ class ForumPost < ActiveRecord::Base
 
 	def send_notifications!
 		users = forum_thread.users.uniq - [user]
+
+		users.each do |user|
+			Notificationmailer.forum_post_notification(user, self).deliver_later
+		end
 	end
 end
