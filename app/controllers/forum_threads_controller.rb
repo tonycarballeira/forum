4960b82e261f3e69eb6ed6902030efe7df8b5898
tonycarballeira <tonycarballeira@gmail.com>
2015-01-20 16:34:53 -0500

    Setup Nested Attributes In Fourmthreads Controller

--- app/controllers/forum_threads_controller.rb
+++ app/controllers/forum_threads_controller.rb
@@ -1,3 +1,25 @@
 class ForumThreadsController < ApplicationController
 	before_action :authenticate_user!, except: [:index, :show]
 	before_sction :set_forum_thread, except: [:index, :new, :create]
+
+	def index
+		@forum_threads = ForumThread.all
+	end	
+
+	def show
+	end	
+
+	def new
+		@forum_thread = ForumThread.new
+		@forum_thread.forum_posts.new
+	end
+
+	def create
+	end
+
+	private
+
+		def set_fourm_thread
+			@forum_thread = ForumThread.find(params[:id])
+		end
+end
