688af0dc9a051a994425be99a2057de60ed3b9c1
tonycarballeira <tonycarballeira@gmail.com>
2015-01-20 17:03:35 -0500

    Added Validation to ForumThread Model

--- app/controllers/forum_threads_controller.rb
+++ app/controllers/forum_threads_controller.rb
@@ -15,6 +15,14 @@ class ForumThreadsController < ApplicationController
 	end
 
 	def create
+		@forum_thread = current_user.forum_threads.new forum_thread_params
+		@forum_thread.forum_posts.first.user_id = current_user.id
+
+		if @forum_thread.save
+			redirect_to @forum_thread
+		else
+			render action: :new
+		end
 	end
 
 	private
@@ -22,4 +30,7 @@ class ForumThreadsController < ApplicationController
 		def set_fourm_thread
 			@forum_thread = ForumThread.find(params[:id])
 		end
+
+		def forum_thread_params
+			params.require(:forum_thread).permit(:subject, forum_posts_attributes: [:body])
 end
--- app/models/forum_thread.rb
+++ app/models/forum_thread.rb
@@ -1,4 +1,9 @@
 class ForumThread < ActiveRecord::Base
 	belongs_to :user
 	has_many :forum_posts
+
+	accepts_nested_attributes_for :forum_posts
+
+	validates :subject, presence: true
+	validates_associated :forum_posts
 end
