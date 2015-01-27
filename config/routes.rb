381e4e80553a7fdb5ad5498c373acc3ebb597149
tonycarballeira <tonycarballeira@gmail.com>
2015-01-26 19:00:41 -0500

    Configured Routes.rb For Likes In ForumPosts

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :forum_threads do
     resources :forum_posts, module: :forum_threads
+      resource :like
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
